6d173a03a92be6cdf2533339963e206d8ad34c49
Jerry <jerryshum2636@gmail.com>
2024-12-01 16:49:27 -0500

    General home page setup

--- app/page.js
+++ app/page.js
@@ -1,3 +1,32 @@
+import Link from "next/link";
+
 export default function Home() {
-   return <div className="">Home page</div>;
+   const username = "Jerry";
+   const tasks = [
+      { id: 1, title: "Complete project report", status: "In Progress" },
+      { id: 2, title: "Review team feedback", status: "Pending" },
+      { id: 3, title: "Update task tracker", status: "Completed" },
+   ];
+
+   return (
+      <div className="px-60 space-y-10">
+         <h1 className=" text-primary-lighter font-bold text-4xl mt-10">
+            Welcome, {username}!
+         </h1>
+         {/* <h2>Track and manage your tasks efficiently using our TaskTracker!</h2> */}
+
+         {tasks ? (
+            <div className="border-2 border-accent-light p-6 rounded-xl">
+               <h2 className="text-primary-light font-semibold text-2xl ">
+                  Current status of your tasks:
+               </h2>
+            </div>
+         ) : (
+            <div>
+               <h2>Start by adding a task.</h2>
+               <Link href={"/task"} />
+            </div>
+         )}
+      </div>
+   );
 }
