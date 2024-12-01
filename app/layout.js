4a983017a1ed203ff83ed93dd08caeb78422703b
Jerry <jerryshum2636@gmail.com>
2024-12-01 13:10:23 -0500

    setting up fonts

--- app/layout.js
+++ app/layout.js
@@ -1,30 +1,22 @@
-import localFont from "next/font/local";
-import "./globals.css";
+import { Inter } from "next/font/google";
 
-const geistSans = localFont({
-   src: "./fonts/GeistVF.woff",
-   variable: "--font-geist-sans",
-   weight: "100 900",
-});
-const geistMono = localFont({
-   src: "./fonts/GeistMonoVF.woff",
-   variable: "--font-geist-mono",
-   weight: "100 900",
+const inter = Inter({
+   subsets: ["latin"],
+   display: "swap",
 });
 
 export const metadata = {
-   title: "Task Tracker",
+   title: {
+      template: "%s | Task Tracker",
+      default: "Welcome | Task Tracker",
+   },
    description: "Part 1 of Jerry's Personal Projects",
 };
 
 export default function RootLayout({ children }) {
    return (
       <html lang="en">
-         <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-         >
-            {children}
-         </body>
+         <body className={`${inter.className} antialiased`}>{children}</body>
       </html>
    );
 }
