a26dd0abf0f2c80e9daa41f0ae02c6084240a10a
Jerry <jerryshum2636@gmail.com>
2024-12-01 18:43:28 -0500

    error styling

--- app/error.js
+++ app/error.js
@@ -2,12 +2,14 @@
 
 export default function Error({ error, reset }) {
    return (
-      <main className="flex justify-center items-center flex-col gap-6 text-primary">
-         <h1 className="text-3xl font-semibold ">Something went wrong!</h1>
-         <p className="text-lg">{error.message}</p>
+      <main className="text-center space-y-6 flex flex-col justify-center items-center h-screen">
+         <h1 className="text-3xl font-semibold text-text-light">
+            Something went wrong!
+         </h1>
+         <p className="text-lg text-text-light">{error.message}</p>
 
          <button
-            className="inline-block text-accent-lighter px-6 py-3 text-lg"
+            className=" bg-accent-500 text-primary-800 px-6 py-3 text-lg text-primary-light bg-primary-darker rounded-xl"
             onClick={reset}
          >
             Try again
