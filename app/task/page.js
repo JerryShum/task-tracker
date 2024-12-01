81c91b5dbd5dc2fedeaca8af37a396726e8e30d9
Jerry <jerryshum2636@gmail.com>
2024-12-01 18:12:08 -0500

    task page

--- app/task/page.js
+++ app/task/page.js
@@ -1,7 +1,16 @@
+import TaskList from "../_components/task/TaskList";
+
 export default function Page() {
    return (
-      <div>
-         <h2>Task Page</h2>
+      <div className="px-60 py-10 space-y-10">
+         <h1 className=" text-primary-light font-bold text-4xl">Tasks</h1>
+
+         <div className="p-6 border-2 border-accent-lighter rounded-xl space-y-6">
+            <h2 className="text-accent-lighter font-bold text-2xl">
+               Task List
+            </h2>
+            <TaskList />
+         </div>
       </div>
    );
 }
