ced8b25b313c93d00749f627026478d0ca72e405
Jerry <jerryshum2636@gmail.com>
2024-12-02 14:13:05 -0500

    button styles

--- app/_components/UI/Button.js
+++ app/_components/UI/Button.js
@@ -1,17 +1,19 @@
-function Button({ onClick, children, type }) {
+"use client";
+
+function Button({ onClickFunction, children, type }) {
    const baseStyle = "px-4 py-2 font-semibold rounded-xl";
    const typeStyles = {
       primary:
          "bg-primary-lighter text-background hover:bg-primary-light duration-300",
       secondary:
-         "bg-accent-lighter text-background hover:bg-accent-light duration-300",
+         "bg-accent-lighter text-background hover:bg-accent-light duration-300 ",
       danger: "bg-red-500 text-white hover:bg-red-700",
    };
 
    const buttonStyle = `${baseStyle} ${typeStyles[type] || ""}`;
 
    return (
-      <button onClick={onClick} className={buttonStyle}>
+      <button onClick={onClickFunction} className={buttonStyle}>
          {children}
       </button>
    );
