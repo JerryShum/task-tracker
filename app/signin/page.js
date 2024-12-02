1b76ffe58c079ae0df01a53158c25963778dd39a
Jerry <jerryshum2636@gmail.com>
2024-12-02 14:11:59 -0500

    sign in page

--- app/signin/page.js
+++ app/signin/page.js
@@ -1,7 +1,20 @@
+"use client";
+
+import Button from "../_components/UI/Button";
+import { signIn, signOut } from "next-auth/react";
+import { auth } from "../_lib/auth";
+import "tailwindcss/tailwind.css"; // Import Tailwind CSS
+
 export default function Page() {
    return (
-      <div>
-         <h1>This is the login page</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+         <h1 className="text-4xl font-bold mb-8">This is the login page</h1>
+         <button
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+            onClick={() => signIn("google")}
+         >
+            Continue with Google
+         </button>
       </div>
    );
 }
