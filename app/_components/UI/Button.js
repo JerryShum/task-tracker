36182b34ef6f8c3de16b1d62fcb7113deca2d8d8
Jerry <jerryshum2636@gmail.com>
2024-12-01 22:18:21 -0500

    Hover animation

--- app/_components/UI/Button.js
+++ app/_components/UI/Button.js
@@ -1,9 +1,11 @@
 function Button({ onClick, children, type }) {
    const baseStyle = "px-4 py-2 font-semibold rounded-xl";
    const typeStyles = {
-      primary: " bg-primary-lighter text-background",
-      secondary: " bg-accent-lighter text-background",
-      danger: "bg-red-500 text-white",
+      primary:
+         "bg-primary-lighter text-background hover:bg-primary-light duration-300",
+      secondary:
+         "bg-accent-lighter text-background hover:bg-accent-light duration-300",
+      danger: "bg-red-500 text-white hover:bg-red-700",
    };
 
    const buttonStyle = `${baseStyle} ${typeStyles[type] || ""}`;
