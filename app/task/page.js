9b9f9f8baa8e6b651e8225e1c0bdb49d0dccc729
Jerry <jerryshum2636@gmail.com>
2024-12-01 22:06:06 -0500

    addtaskform

--- app/task/page.js
+++ app/task/page.js
@@ -1,9 +1,12 @@
+import AddTaskForm from "../_components/task/AddTaskForm";
 import TaskList from "../_components/task/TaskList";
 
 export default function Page() {
    return (
       <div className="px-60 py-10 space-y-10">
          <h1 className=" text-primary-light font-bold text-4xl">Tasks</h1>
+
+         <AddTaskForm />
 
          <div className="p-6 border-2 border-accent-lighter rounded-xl space-y-6">
             <h2 className="text-accent-lighter font-bold text-2xl">
