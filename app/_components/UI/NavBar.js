dbb4be79c4aeb41c247611fe56a8995309b15a9f
Jerry <jerryshum2636@gmail.com>
2024-12-02 21:13:07 -0500

    removing links in navbar

--- app/_components/UI/NavBar.js
+++ app/_components/UI/NavBar.js
@@ -9,6 +9,14 @@ function NavBar() {
                   Login
                </Link>
             </li>
+            <li>
+               <Link
+                  href="/signup"
+                  className="hover:bg-blue-600 hover:text-accent-lightest duration-300 bg-blue-500 rounded-xl text-black font-semibold p-2 py-3"
+               >
+                  Get Started
+               </Link>
+            </li>
          </ul>
       </nav>
    );
