4c94aca5f897bcd2cfd893b22db0addcf42e2919
Jerry <jerryshum2636@gmail.com>
2024-12-06 22:53:01 -0500

    fixed routing

--- app/_components/UI/Logo.js
+++ app/_components/UI/Logo.js
@@ -4,7 +4,7 @@ import logo from "@/public/logo.png";
 
 function Logo() {
    return (
-      <Link href={"/"} className="flex items-center gap-4">
+      <Link href={"/application"} className="flex items-center gap-4">
          <Image src={logo} alt="Task Tracker Logo" height={40} width={40} />
 
          <span className="text-xl font-bold text-primary-lighter">
